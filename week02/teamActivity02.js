2a02f0ec831018e19bf80a3e3cc0050ddfc545ce
jsvilchis <vil15009@byui.edu>
2018-01-17 02:16:16 -0700

    added window

--- week02/teamActivity02.js
+++ week02/teamActivity02.js
@@ -4,6 +4,6 @@ function buttonAlert() {
 
 function changeColor(color, color_change) {
     //document.getElementById('hover_alert').style.color = color;
-    document.getElementById('hover_alert').innerHTML = color;
+    window.document.getElementById('hover_alert').innerHTML = color;
 
 }
