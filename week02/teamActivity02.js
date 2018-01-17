19f8b5dd5aea3d401d93cbbae40b1d1087902fc1
jsvilchis <vil15009@byui.edu>
2018-01-17 01:53:09 -0700

    Checking value

--- week02/teamActivity02.js
+++ week02/teamActivity02.js
@@ -5,4 +5,5 @@ function buttonAlert() {
 function changeColor(color, color_change) {
     //document.getElementById('hover_alert').style.color = color;
     color_change = color;
+    console.log(color_change);
 }
