9a73fcb059ca292ff343279b8b374ccf620ede31
jsvilchis <vil15009@byui.edu>
2018-01-17 12:13:29 -0700

    Added a log

--- week02/teamActivity02.js
+++ week02/teamActivity02.js
@@ -7,6 +7,6 @@ function changeColor() {
     var div_color = document.getElementById('hover_alert');
 
     var color_change = document.getElementById('form1').elements.namedItem('change_color_button').value;
-
+    console.log(color_change);
     document.getElementById('hover_alert').style.backgroundColor = color_change;
 }
