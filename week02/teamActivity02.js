486a00bddfaa477c0242da4469a061fa457b53a4
jsvilchis <vil15009@byui.edu>
2018-01-17 19:46:02 -0700

    Working on jquery

--- week02/teamActivity02.js
+++ week02/teamActivity02.js
@@ -10,6 +10,8 @@ function changeColor() {
     div_to_change.style.backgroundColor = color_change;
 }
 
-$("#jquery_click_me_button").click(function () {
-    $("#css_bold").hide();
+$(document).ready(function () {
+    $("#jquery_click_me_button").click(function () {
+        $("#css_bold").hide();
+    });
 });
